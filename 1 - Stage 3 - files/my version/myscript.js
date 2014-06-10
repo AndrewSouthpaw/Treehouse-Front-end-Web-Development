1896746f900200917a726507060d43314c6804c4
Andrew Smith <andrewsmith@alumni.stanford.edu>
2014-06-10 17:10:35 +0100

    commit

---
+++ 1 - Stage 3 - files/my version/myscript.js
@@ -0,0 +1,18 @@
+
+console.log("Hello from myscript.js");
+var me = {
+	first_name: "Andrew",
+	last_name: "Smith",
+	"Employee Number": 1
+}
+console.log(me.first_name);
+console.log(me.last_name);
+console.log(me["Employee Number"]);
+console.log(me["last_name"]);
+console["log"](me);
+
+var key = first_name;
+
+console.log(me[key]);
+
+
